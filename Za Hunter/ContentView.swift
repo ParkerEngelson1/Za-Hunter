0f51cb019c8f4ae4af857e9f953102688334a8cb
Parker <Parker.engelson09@gmail.com>
2026-02-17 08:44:34 -0800

    Initial Commit

---
+++ Za Hunter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Za Hunter
+//
+//  Created by Parker Engelson on 2/17/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Za Hunter/Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Za_HunterApp.swift
+//  Za Hunter
+//
+//  Created by Parker Engelson on 2/17/26.
+//
+
+import SwiftUI
+
+@main
+struct Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
